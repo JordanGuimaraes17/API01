a36db5233f4139ae94383645ba5e171806ddd9d7
Jordan Guimarães <jordanggomes@gmail.com>
2023-12-15 15:19:40 -0300

    Preparando o Deploy

--- src/providers/diskStorage.js
+++ src/providers/diskStorage.js
@@ -3,8 +3,16 @@ const path = require('path')
 const uploadConfig = require('../configs/upload')
 
 class DiskStorage {
+  static ERROR_INVALID_EXTENSION = 'Extensão de arquivo não permitida'
+  static ERROR_DELETE_FILE = 'Erro ao excluir o arquivo'
   isValidExtension(file) {
-    const allowedExtensions = ['.png', '.jpg', '.jpeg'] // Adicione as extensões permitidas aqui
+    // Adicione as extensões permitidas na configuração
+    const allowedExtensions = uploadConfig.allowedExtensions || [
+      '.png',
+      '.jpg',
+      '.jpeg'
+    ]
+
     const extname = path.extname(file).toLowerCase()
     console.log('Extensão do arquivo:', extname)
     return allowedExtensions.includes(extname)
@@ -12,13 +20,15 @@ class DiskStorage {
 
   async saveFile(file) {
     if (!this.isValidExtension(file)) {
-      throw new Error('Extensão de arquivo não permitida')
+      throw new Error(DiskStorage.ERROR_INVALID_EXTENSION)
     }
 
-    await fs.promises.rename(
-      path.resolve(uploadConfig.TMP_FOLDER, file),
-      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
-    )
+    const sourcePath = path.resolve(uploadConfig.TMP_FOLDER, file)
+    const destinationPath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+
+    await fs.promises.rename(sourcePath, destinationPath)
+    console.log(`Arquivo salvo em: ${destinationPath}`)
+
     return file
   }
 
@@ -33,8 +43,8 @@ class DiskStorage {
       await fs.promises.access(filePath, fs.constants.F_OK)
       await fs.promises.unlink(filePath)
     } catch (error) {
-      // Lidar com outros erros, se necessário
-      return
+      console.error('Erro ao excluir o arquivo:', error)
+      // Lançar o erro ou lidar de alguma outra maneira, dependendo dos requisitos
     }
   }
 }
